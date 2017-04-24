67e4e773f56fef20331f89fb8f15d26fe0151ae9
Simen Hellem <simenhellem@dhcp-10-24-30-214.wlan.ntnu.no>
2017-04-24 15:21:53 +0200

    Made the imageReader into a class, reads all the images of a letter

--- imageReading.py
+++ imageReading.py
@@ -1,18 +1,43 @@
-import os, sys
 from PIL import Image
 
-im = Image.open("/Users/simenhellem/Documents/chars74k-lite/a/a_0.jpg")
-x = 3
-y = 4
+### After reading, all images can be feetched from the imageVector ###
 
-print(im.format, im.size, im.mode)
-im.show()
-pix = im.load()
+class ImageReader:
 
-imageList = []
-for i in range(20):
-    for j in range(20):
-        imageList.append(pix[i, j])
+    def __init__(self, imageFolder):
+        self.imageFolder = imageFolder
+        self.sizeX = 20
+        self.sizeY = 20
+        self.imageVector = []
 
-print(imageList)
+    def readImage(self, letter):
+        source = self.imageFolder + letter
+        nextImage = True
+        count = 0
+        while(nextImage):
+            try:
+                lett = "a_" + str(count) + ".jpg"
+                newSource = source
+                newSource += lett
+                im = Image.open(newSource)
+                pix = im.load()
+                imageList = []
+                for i in range(self.sizeX):
+                    for j in range(self.sizeY):
+                        imageList.append(pix[i, j])
+                self.imageVector.append(imageList)
+
+            except IOError:
+                nextImage = False
+
+
+            count += 1
+
+
+reader = ImageReader("/Users/simenhellem/Documents/chars74k-lite/")
+reader.readImage("a/")
+
+
+
+
 
