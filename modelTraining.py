ab564e69729aa109e0087599ae945501914d6545
Simen Hellem <simenhellem@Simens-MacBook-Pro.local>
2017-04-25 09:25:13 +0200

    Cleaned the classifier

--- modelTraining.py
+++ modelTraining.py
@@ -1,60 +1,81 @@
+from imageReading import ImageReader
+import math
+
+### ONE VS REST ###
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
+
+### FOREST ###
 from sklearn.ensemble import RandomForestClassifier
-from imageReading import ImageReader
 from sklearn import tree
+
+## NAIVE BAYES ##
 from sklearn.naive_bayes import GaussianNB
-import math
-from sklearn.preprocessing import LabelBinarizer
 
 
-
-array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+## CONSTANTS ##
+ARRAY = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+TESTSET = 0.2
 y = []
 X = []
 reader = ImageReader("/Users/simenhellem/Documents/chars74k-lite/")
-count = 0
 
+
+## FOR TESTING ###
 testSetRemember = []
 testSetAll = []
 
+def prepareTrainandTestSet():
+    for i in range(len(ARRAY)):
+        input = ARRAY[i] + "/"
+        reader.readImage(input)
+        print(len(reader.imageVector))
+        numberTest = int(math.floor(len(reader.imageVector) * TESTSET))
+        for j in range(len(reader.imageVector)):
+            if(j < len(reader.imageVector) - numberTest):
+                y.append(i)
+                X.append(reader.imageVector[j])
+            else:
+                testSetAll.append(reader.imageVector[j])
+                testSetRemember.append(i)
+        reader.clearVector()
 
-for i in range(len(array)):
-    input = array[i] + "/"
-    reader.readImage(input)
-    print(len(reader.imageVector))
-    numberTest = int(math.floor(len(reader.imageVector) * 0.2))
-    for j in range(len(reader.imageVector)):
-        if(j < len(reader.imageVector) - numberTest):
-            y.append(i)
-            X.append(reader.imageVector[j])
-        else:
-            testSetAll.append(reader.imageVector[j])
-            testSetRemember.append(i)
-    reader.clearVector()
+
+def classify():
+    results = clf.predict(testSetAll)
+    missedCount = 0
+    for i in range(len(results)):
+        if results[i] != testSetRemember[i]:
+            print("You thought it was", results[i])
+            print("But it was:", testSetRemember[i])
+            missedCount += 1
+    total = float(missedCount) / float(len(results))
+    print("you missed: ", total)
 
 
-#classif = OneVsRestClassifier(estimator=SVC(random_state=0))
-#classif = OneVsRestClassifier(LinearSVC(C=100.)).fit(X, y)
+prepareTrainandTestSet()
+
+### DIFFERENT CLASSIFIERS ###
+#clf = OneVsRestClassifier(estimator=SVC(random_state=0))
+#clf = OneVsRestClassifier(LinearSVC(C=100.)).fit(X, y)
 #clf = RandomForestClassifier(n_estimators=40)
 #clf = GaussianNB()
 clf = tree.DecisionTreeClassifier()
+
+### FIT TO TRAINING DATA ###
 clf.fit(X, y)
 
 print("built model")
-#classif.fit(X, y).predict(X)
 print("going for prediction")
-results = clf.predict(testSetAll)
-missedCount = 0
-length = len(results)
-for i in range(len(results)):
-    print(results[i])
 
-for i in range(len(results)):
-    if results[i] != testSetRemember[i]:
-        print("You thought it was", results[i])
-        print("But it was:", testSetRemember[i])
-        missedCount += 1
-total = float(missedCount)/float(length)
-print("you missed: ", total)
+### TEST ON TEST SET ###
+classify()
+
+
+
+
+
+
+
+
