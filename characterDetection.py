c22f07573f95acb70453803a3c57863fe59964ce
Carl Andreas Julsvoll <julsvoll@gmail.com>
2017-04-27 15:12:52 +0200

    kong sverråå

--- characterDetection.py
+++ characterDetection.py
@@ -6,17 +6,20 @@ import string
 import numpy as np
 from PIL import Image
 from imagePreProcessing import digitalImagePrep
+import math
 
 #modelTraining.classify(matrix of pixels)
 
 ARRAY = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 reader = ImageReader("detection-images/")
-imageName = "detection-1.jpg"
+imageName = "detection-2.jpg"
 reader.readImageFromFileName(imageName)
 
 patchSize = 20 #x20
 #print reader.imageVector
-numberToDetect = 8
+numberToDetect = 50
+maxPercentageWhite =0.3
+stepSize = 2
 
 #bruk classify som tar inn imageMatrix og returnerer et element i ARRAY
 
@@ -30,27 +33,51 @@ def slidingWindowDetection(clf,scaler,imageMatri,outputPic):
 	count = 0
 	pathcesToKeep = []
 	worstAmongNumbersToCheck = 0
-	for row in range(0,rows-patchSize,7):
-		for col in range(0,cols-patchSize,7):
+	for row in range(0,rows-patchSize,stepSize):
+		for col in range(0,cols-patchSize,stepSize):
 			#make imageVector
 			imageVector = imageMatrix[row:row+patchSize,col:col+patchSize].reshape(1,400)[0]
 			#imageVector = digitalImagePrep(imageVector, "fname",2).reshape(1,400)[0]*255
-			
-			if imageVector.max()==imageVector.min():
+			unique, counts = np.unique(imageVector, return_counts=True)
+			b = dict(zip(unique, counts))
+			if 255 in b.keys() and b[255]> 20*20*maxPercentageWhite:
 				continue
-			scaler.transform([imageVector])
 			a = clf.predict_proba([imageVector])[0][:-1]
-			
+
 			best = max(a)
+			
 			pathcesToKeep.append([[row,col],best])
 			count += 1
 			if count % 10 == 0:
 				print count
-	#find top numberToDetect best:
+	
 
+	#find top numberToDetect best:
 	pathcesToKeep.sort(key = lambda x : x[1])
-	pathcesToKeep =  pathcesToKeep[-1-numberToDetect:-1]
-	for coor,prob in pathcesToKeep:
+	patchesKept = []
+	count = 0
+	print len(pathcesToKeep)
+	while count != numberToDetect:
+		try:
+			new = pathcesToKeep[-1]
+			coor,prob = new
+			row,col = coor[0],coor[1]
+			pathcesToKeep = pathcesToKeep[:-1]
+			notAllowed = False
+			#check if patch is close to already added patch
+			for coor2,p2 in patchesKept:
+				r2,c2 = coor2[0],coor2[1]
+				if abs(r2-row) < patchSize and abs(c2-col)  < patchSize:
+					notAllowed = True
+					break
+			if notAllowed:
+				None
+			else:
+				count+=1
+				patchesKept.append(new)
+		except:
+			break
+	for coor,prob in patchesKept:
 		row,col = coor[0],coor[1]
 		
 		for c in range(col,col+patchSize):
